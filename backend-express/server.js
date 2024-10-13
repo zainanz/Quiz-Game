8d085931d087a26f9af4a12c9bacf505db451afc
Zainan Ali <zainanzaher09@gmail.com>
2024-10-13 16:51:47 +0100

    initialized the socket io in another folder instaed of passing it as a prop. - To prevent unexpected behaviors

--- backend-express/server.js
+++ backend-express/server.js
@@ -11,11 +11,27 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin:"http://localhost:3000"
+    origin:"http://localhost:3000",
+    methods: ["GET", "POST"]
   }
 })
 
+
+
 const connectSockets = new Set();
+
+let users = []
+
+let gameRunning = false;
+
+const game = () => {
+  let winner = ""
+  setInterval(() => {
+    io.emit("question")
+  }, 10000);
+
+}
+
 
 io.on("connection", (socket) => {
   connectSockets.add(socket.id);
@@ -24,11 +40,35 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     connectSockets.delete(socket.id)
     console.log(`${socket.id} left the server.`)
+    users = users.filter( user => user.socket_id === socket.id);
+    console.log(users);
+
   })
 
   socket.on("send_message", (data) => {
-    console.log(data);
     socket.broadcast.emit("incoming_messages", data)
+  })
+
+  socket.on("register", (data) => {
+    const userInstance = {
+      [data]: 0,
+      socket_id: socket.id
+    };
+    users.push(userInstance)
+    console.log(userInstance);
+    console.log(users);
+
+    if (users.length > 1 && !gameRunning){
+      console.log("run");
+      console.log(socket);
+
+      socket.emit("server_message", "Game is starting soon..")
+    } else {
+      console.log("run2");
+      console.log(users.length);
+
+      socket.emit("server_message", `Need ${2 - users.length} more player(s) to start.`)
+    }
   })
 })
 
